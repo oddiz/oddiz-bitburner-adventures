c42e9e28629ab3d9ba4dca215c253a195104a656
Kaan Şarkaya <kaansarkaya@gmail.com>
2022-07-08 16:41:05 +0300

    feat (backdoor automation): working with manual enter presses

--- src/utils/backdoorFactionServers.ts
+++ src/utils/backdoorFactionServers.ts
@@ -1,14 +1,22 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { findRouteTo } from "/utils/findRouteTo";
 import { sleep } from "/utils/sleep";
 import { NS, Server } from "/typings/Bitburner";
+import { KeyboardEvent } from "react";
+import { notStrictEqual } from "assert";
 
 const factionServers = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z"];
+
+export async function main(ns: NS) {
+    ns.disableLog("ALL");
+    await backdoorFactionServers(ns);
+}
 export async function backdoorFactionServers(ns: NS) {
     const facSvInfos = factionServers.map((hostname) => ns.getServer(hostname));
 
     for (const target of facSvInfos) {
         const targetHostname = target.hostname;
-        if (target.requiredHackingSkill < ns.getPlayer().hacking) {
+        if (target.requiredHackingSkill > ns.getHackingLevel()) {
             console.warn(
                 `Can't backdoor server ${targetHostname}, needed hacking skill: ${target.requiredHackingSkill}`
             );
@@ -17,42 +25,69 @@ export async function backdoorFactionServers(ns: NS) {
         }
 
         //connect to server
-        await sendTerminalCommand("home");
+        await sendTerminalCommand(ns, "home");
         const routeToServer = findRouteTo(ns, targetHostname);
 
         if (!routeToServer) console.warn("Couldn't find route to server");
 
         for (const server of routeToServer) {
-            await sendTerminalCommand(`connect ${server}`);
+            await sendTerminalCommand(ns, `connect ${server}`);
         }
 
         const backdoorTime = getBackdoorTime(ns, target);
 
-        await sendTerminalCommand("backdoor");
+        await sendTerminalCommand(ns, "backdoor");
         await sleep(backdoorTime);
     }
 }
 
-async function sendTerminalCommand(query: string) {
+async function sendTerminalCommand(ns: NS, query: string) {
     const cheatyWindow = eval("window") as Window;
     const terminalInput = cheatyWindow.document.getElementById("terminal-input") as HTMLInputElement;
 
+    let enterPressed = false;
     if (!terminalInput) {
         console.warn("Please navigate to terminal page!");
         return;
     }
 
-    terminalInput.value = query;
-    cheatyWindow.dispatchEvent(
-        new KeyboardEvent("keydown", {
-            key: "Enter",
-        })
-    );
+    const onKeyUpFunc = (e: globalThis.KeyboardEvent) => {
+        if (e.key === "Enter") {
+            enterPressed = true;
+        }
+    };
 
-    await sleep(100);
+    triggerInputChange(terminalInput, query);
+
+    cheatyWindow.document.addEventListener("keyup", onKeyUpFunc);
+
+    while (!enterPressed && ns.scriptRunning("/utils/backdoorFactionServers.js", "home")) {
+        await sleep(100);
+    }
+
+    cheatyWindow.document.removeEventListener("keyup", onKeyUpFunc);
+
     return;
 }
 
 function getBackdoorTime(ns: NS, server: Server) {
     return ns.getHackTime(server.hostname) / 4; //got this info from bitburner source code
 }
+
+const inputTypes = [window.HTMLInputElement, window.HTMLSelectElement, window.HTMLTextAreaElement];
+
+export const triggerInputChange = (node, value = "") => {
+    if (!node) throw Error("Error in triggering or changing input");
+    // only process the change on elements we know have a value setter in their constructor
+
+    // @ts-ignore
+    if (inputTypes.indexOf(node.__proto__.constructor) > -1) {
+        // @ts-ignore
+        const setValue = Object.getOwnPropertyDescriptor(node.__proto__, "value").set;
+        const event = new Event("input", { bubbles: true });
+
+        // @ts-ignore
+        setValue.call(node, value);
+        node.dispatchEvent(event);
+    }
+};
